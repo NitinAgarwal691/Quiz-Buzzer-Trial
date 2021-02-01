263affadb12678be1651994e3c57295feaf07cc9
NitinAgarwal691 <73051793+NitinAgarwal691@users.noreply.github.com>
2021-02-01 16:02:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// import firebase from 'firebase';
+
+//  const firebaseConfig = {
+//     apiKey: "AIzaSyCwpdtqop-4vsG_LWHK-GteB397O_CEw8I",
+//     authDomain: "quiz-app-41d93.firebaseapp.com",
+//     databaseURL: "https://quiz-app-41d93-default-rtdb.firebaseio.com",
+//     projectId: "quiz-app-41d93",
+//     storageBucket: "quiz-app-41d93.appspot.com",
+//     messagingSenderId: "968566403338",
+//     appId: "1:968566403338:web:de2ed64b9fd2542b7ecaf3",
+//     measurementId: "G-9825KT9M6B"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+// export default firebase.database();
